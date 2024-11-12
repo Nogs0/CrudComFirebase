82e28f38b99d26ba0220b2002d0cdeb2f694ac55
Nogs0 <joaoguinogueira04@gmail.com>
2024-11-12 00:36:05 -0300

    melhorias no estilo e crud em funcionamento

--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,17 @@
 const modalCreateOrEdit = document.getElementById(`modal-create-or-edit`);
+const url = `https://firestore.googleapis.com/v1/projects/mistcar-3cec6/databases/(default)/documents/Carros`;
+const inputMarca = document.getElementById('marca');
+const inputModelo = document.getElementById('modelo');
+const inputCor = document.getElementById('cor');
+const inputPlaca = document.getElementById('placa');
+const inputAno = document.getElementById('ano');
+const inputOdometro = document.getElementById('odometro');
+const inputItemId = document.getElementById('item_id');
 
 async function buscarCarros() {
     try {
         // Faz a requisição para a API
-        const resposta = await fetch("https://firestore.googleapis.com/v1/projects/mistcar-3cec6/databases/(default)/documents/Carros");
+        const resposta = await fetch(url);
         const carros = await resposta.json();
         // Seleciona o corpo da tabela
         const tabelaCorpo = document.getElementById("tabelaCarros").querySelector("tbody");
@@ -14,10 +22,7 @@ async function buscarCarros() {
         // Itera sobre os dados e cria uma nova linha para cada item
         for (let i = 0; i < carros.documents.length; i++) {
             let item = carros.documents[i];
-            console.log(item)
-            // dados.forEach(item => {
             const linha = document.createElement("tr");
-
             // Cria e insere células na linha
             linha.innerHTML = `
                 <td>${i + 1}</td>
@@ -51,20 +56,25 @@ async function buscarCarros() {
 
             let botaoMenu = document.getElementById(`itemButtonMenu_${i}`);
             botaoMenu.addEventListener('click', () => {
-                if (!menu.classList.contains('show'))
-                    menu.classList.add('show');
+                if (!menu.classList.contains('show-menu')) {
+                    const ativos = document.querySelectorAll(".show-menu");
+                    ativos.forEach(menuAtivo => menuAtivo.classList.remove('show-menu'));
+
+                    menu.classList.add('show-menu');
+                }
                 else
-                    menu.classList.remove('show');
+                    menu.classList.remove('show-menu');
+                console.log(menu.classList)
             });
 
             let botaoView = document.getElementById(`itemButtonView_${i}`);
             botaoView.addEventListener('click', () => {
-                visualizarItem(item.fields);
+                visualizarItem(item);
             });
 
             let botaoEdit = document.getElementById(`itemButtonEdit_${i}`);
             botaoEdit.addEventListener('click', () => {
-                editarItem(item.fields);
+                editarItem(item);
             });
 
             let botaoDelete = document.getElementById(`itemButtonDelete_${i}`);
@@ -80,6 +90,12 @@ async function buscarCarros() {
 
 buscarCarros();
 
+function criarCarro() {
+    clearForm();
+    let tituloModal = document.getElementById('tituloModal');
+    tituloModal.textContent = "Criando carro";
+    modalCreateOrEdit.classList.add('show-modal');
+}
 function visualizarItem(item) {
     let tituloModal = document.getElementById('tituloModal');
     tituloModal.textContent = "Visualizando carro";
@@ -91,6 +107,7 @@ function editarItem(item) {
     let tituloModal = document.getElementById('tituloModal');
     tituloModal.textContent = "Editando carro";
     modalCreateOrEdit.classList.add('show-modal');
+
     fillForm(item)
 }
 
@@ -99,35 +116,120 @@ function excluirItem(index) {
 }
 
 function fillForm(item, isView) {
-    let inputMarca = document.getElementById('marca');
-    inputMarca.value = item.MARCA.stringValue;
+    let fields = item.fields;
+    inputItemId.value = item.name.split(`/`)[item.name.split(`/`).length - 1];
+
+    inputMarca.value = fields.MARCA.stringValue;
     inputMarca.disabled = isView;
 
-    let inputModelo = document.getElementById('modelo');
-    inputModelo.value = item.MODELO.stringValue;
+    inputModelo.value = fields.MODELO.stringValue;
     inputModelo.disabled = isView;
 
-    let inputCor = document.getElementById('cor');
-    inputCor.value = item.COR.stringValue;
+    inputCor.value = fields.COR.stringValue;
     inputCor.disabled = isView;
 
-    let inputPlaca = document.getElementById('placa');
-    inputPlaca.value = item.PLACA.stringValue;
+    inputPlaca.value = fields.PLACA.stringValue;
     inputPlaca.disabled = isView;
 
-    let inputAno = document.getElementById('ano');
-    inputAno.value = item.ANO.stringValue;
+    inputAno.value = fields.ANO.stringValue;
     inputAno.disabled = isView;
 
-    let inputOdometro = document.getElementById('odometro');
-    inputOdometro.value = item.ODOMETRO.stringValue;
+    inputOdometro.value = fields.ODOMETRO.stringValue;
     inputOdometro.disabled = isView;
 }
 
+function clearForm() {
+    inputItemId.value = ``;
+    inputMarca.value = ``;
+    inputModelo.value = ``;
+    inputCor.value = ``;
+    inputPlaca.value = ``;
+    inputAno.value = ``;
+    inputOdometro.value = ``;
+}
+
 function cancelar() {
-    modalCreateOrEdit.classList.remove('show-modal')
+    clearForm();
+    closeModal();
 }
 
 function salvar() {
+    if (!dadosSaoValidos()) {
+        alert(`Por favor, preencha os dados corretamente!`);
+        return;
+    }
 
+    if (isNotEmpty(inputItemId.value))
+        update();
+    else
+        create();
+}
+
+function create() {
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({
+            fields: {
+                MARCA: { stringValue: inputMarca.value },
+                MODELO: { stringValue: inputModelo.value },
+                COR: { stringValue: inputCor.value },
+                PLACA: { stringValue: inputPlaca.value },
+                ANO: { stringValue: inputAno.value },
+                ODOMETRO: { stringValue: inputOdometro.value },
+            }
+        })
+    })
+        .then((response) => response.json)
+        .then(() => {
+            alert(`Carro criado com sucesso!`);
+        })
+        .then(() => {
+
+            closeModal();
+            buscarCarros();
+        })
+        .catch((error) => console.error(error))
+}
+
+function update() {
+    fetch(url + '/' + inputItemId.value, {
+        method: 'PATCH',
+        body: JSON.stringify({
+            fields: {
+                MARCA: { stringValue: inputMarca.value },
+                MODELO: { stringValue: inputModelo.value },
+                COR: { stringValue: inputCor.value },
+                PLACA: { stringValue: inputPlaca.value },
+                ANO: { stringValue: inputAno.value },
+                ODOMETRO: { stringValue: inputOdometro.value },
+            }
+        })
+    })
+        .then((response) => response.json)
+        .then(() => {
+            alert(`Carro atualizado com sucesso!`);
+        })
+        .then(() => {
+
+            closeModal();
+            buscarCarros();
+        })
+        .catch((error) => console.error(error))
+}
+
+function dadosSaoValidos() {
+    return isNotEmpty(inputAno.value) &&
+        isNotEmpty(inputMarca.value) &&
+        isNotEmpty(inputModelo.value) &&
+        isNotEmpty(inputOdometro.value) &&
+        isNotEmpty(inputPlaca.value) &&
+        isNotEmpty(inputCor.value);
+}
+
+function isNotEmpty(input) {
+    return input != ``;
+}
+
+function closeModal() {
+    modalCreateOrEdit.classList.remove('show-modal')
 }
